c6a7866cdf3ece64f9d7d88a6614f301f080f003
Kamal Rhrabla <kamalghrabla@gmail.com>
2023-02-27 22:16:07 +0100

    struct the dirs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,11 +3,19 @@ import { UserModule } from './users/user.module';
 import config from "./config/keys";
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdminsModule } from './admins/admins.module';
+import { QuestionController } from './questions/question.controller';
+import { QuestionService } from './question/question.service';
+import { QuestionModule } from './question/question.module';
+import { QuestionsService } from './questions/questions.service';
+import { QuestionsModule } from './questions/questions.module';
+import { AnswersController } from './answers/answers.controller';
+import { AnswersService } from './answers/answers.service';
+import { AnswersModule } from './answers/answers.module';
 
 @Module({
-  imports: [UserModule, AdminsModule, MongooseModule.forRoot(config.mongoURI)],
-  controllers: [],
-  providers: [],
+  imports: [UserModule, AdminsModule, MongooseModule.forRoot(config.mongoURI), QuestionModule, QuestionsModule, AnswersModule],
+  controllers: [QuestionController, AnswersController],
+  providers: [QuestionService, QuestionsService, AnswersService],
 })
 export class AppModule {}
  
